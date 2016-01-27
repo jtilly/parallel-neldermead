3b95a8bf321497f06a20c4433ec321ca46d81856
Jan Tilly <jantilly@gmail.com>
2016-01-27 08:44:57 -0500

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -33,7 +33,7 @@ mpirun -np 2 ./LeeWiswall.out 20
 echo "\n>>>>> This takes a while."
 
 echo "\n\n============================================"
-echo "Parallel implementations with four workers"
+echo "Parallel implementations with three workers"
 echo "============================================"
 mpirun -np 3 ./DistParNelderTest.out 20 1
 mpirun -np 3 ./LeeWiswall.out 20 
